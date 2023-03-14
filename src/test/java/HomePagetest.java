88952ecddb6a0890c4aaa05d362154b1e372c173
magda <magda.wojtysiak@gmail.com>
2023-03-14 21:13:37 +0000

    Initial commit

---
+++ src/test/java/HomePagetest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePagetest {
+
+    private WebDriver driver;
+    private WebElement element;
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void ShouldReturnCorrectPageTitle() {
+        // Your test logic here
+        driver.get("http://automationpractice.pl/index.php");
+
+        System.out.println(driver.getTitle());
+
+
+    }
+
+
+}
